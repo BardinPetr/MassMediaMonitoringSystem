dd5709cd569dbe0281b4dc3e554d2e890d30d431
Danula <dva.straus@yandex.ru>
2019-07-06 15:48:03 +0400

    Auto stash before merge of "merge_test_1" and "origin/merge_test_1"

--- DB_VKAPI.py
+++ DB_VKAPI.py
@@ -1,6 +1,6 @@
 import YandexNewsRSS
 from BD.DB import DB
-
+import vkapi.simple_parser
 
 class DataSourceProvider:
     def __init__(self):
@@ -9,12 +9,23 @@ class DataSourceProvider:
         self.data_base = DB()
 
     def save_posts(self, quest, count):
-        self.data_base.add_posts(self.VK.get_post(quest, count))
+        data_vk=self.VK.get_post(quest, count)
+        comments=[]
+        posts=[]
+        for i in range(count):
+            posts.append(data_vk[i][0])
+            for j in range(len(data_vk[i][1])):
+                print(data_vk[i][1])
+                if data_vk[i][1][j]['text']!='0':
+                    comments.append(data_vk[i][1][j])
+        if len(comments)>0:
+            self.data_base.add_comments(comments)
+        print(data_vk[0])
+        self.data_base.add_posts(posts)
 
     def save_YNews(self):
         self.data_base.add_news(self.YandexN.get_news(1))
 
 
 c = DataSourceProvider()
-c.save_posts("как житуха", 200)
-c.save_YNews()
+c.save_posts("бабка", 200)
