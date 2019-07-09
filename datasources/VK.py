6768c20be66e09842200efc66b8ae23753e3623d
mmkuznecov <mmkuznecov2002@gmail.com>
2019-07-10 00:34:53 +0300

    sfsdf

--- datasources/VK.py
+++ datasources/VK.py
@@ -1,15 +1,8 @@
 import vk
 
-from datasources import options
-
-
 class VKAPI:
     def __init__(self, version="9.52"):
-        self.version = version
-        session = vk.AuthSession(app_id=options.ID,
-                                 user_login=options.LOGIN,
-                                 user_password=options.PASSWORD,
-                                 scope='wall, fields, messages')
+        session = vk.Session(access_token='c47c0269c47c0269c47c0269f2c41791c4cc47cc47c026999589348acc69844875e4dad'))
         self.vkapi = vk.API(session)
 
     @staticmethod
@@ -20,18 +13,10 @@ class VKAPI:
         post_news = {"query": request,
                      "id": wall_item['id'],
                      "owner_id": wall_item['owner_id'],
-                     "from_id": wall_item['from_id'],
-                     "views_count": wall_item['views']['count'],
-                     "likes_count": wall_item['likes']['count'],
-                     "comments_count": wall_item['comments']['count'],
-                     "reposts_count": wall_item['reposts']['count'],
                      "text": wall_item['text']}
         return post_news
 
     def get_post(self, quest, count):
-        newsfeed = self.vkapi.newsfeed.search(q=quest,
-                                              count=count,
-                                              filters='post ',
-                                              v=self.version)
+        newsfeed = self.vkapi.newsfeed.search(q=quest, count=count, filters='post', v=self.version)
         list_news = [self.create_structure(newsfeed['items'][i], quest) for i in range(count)]
         return list_news
---
+++ datasources/new_vk.py
@@ -0,0 +1,16 @@
+import vk
+from datetime import datetime
+
+class GetPosts:
+    def get_thousend_posts(self,query):
+        session = vk.Session(access_token='c47c0269c47c0269c47c0269f2c41791c4cc47cc47c026999589348acc69844875e4dad')
+        vkapi = vk.API(session)
+        newsfeed = []
+        for i in range(5):
+            feed = vkapi.newsfeed.search(q=query, count=200, filters='post', v=5.12, offset=i*200)
+            for news in feed['items']:
+                newsfeed.append({'text':news['text'], 'date':news['date']})
+        
+        epoch = newsfeed[0]['date']-newsfeed[-1]['date']
+
+        return [newsfeed,epoch]
