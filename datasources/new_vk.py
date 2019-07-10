8cff470e0e4c4b95c736247e5890dfd39bf825fb
mmkuznecov <mmkuznecov2002@gmail.com>
2019-07-10 10:07:07 +0300

    df

--- datasources/new_vk.py
+++ datasources/new_vk.py
@@ -1,6 +1,5 @@
 import vk
-from datetime import datetime
-import time
+
 class GetPosts:
 
     def get_thousend_posts(self,query):
@@ -8,7 +7,7 @@ class GetPosts:
         vkapi = vk.API(session)
         newsfeed = []
         for i in range(5):
-            feed = vkapi.newsfeed.search(q=query, count=150, filters='post', v=5.12, offset=i*200)
+            feed = vkapi.newsfeed.search(q=query, count=200, filters='post', v=5.12, offset=i*200)
             for news in feed['items']:
                 newsfeed.append({'text':news['text'], 'date':news['date'],'query':query})
         
