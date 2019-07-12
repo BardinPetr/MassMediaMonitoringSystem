67a245c4707a16b8c3ed551269088f25dbec7df9
mmkuznecov <mmkuznecov2002@gmail.com>
2019-07-12 16:05:12 +0300

    Merge

--- make_full.py
+++ make_full.py
@@ -1,9 +1,9 @@
-from datasources.new_vk import GetPosts
+from datasources.VK import VK
 
 from DB.DB import DB
 
-g = GetPosts()
+g = VK()
 
 a = DB()
-a.add_posts(g.get_thousend_posts('Гагра'))
+a.add_posts(g.get_thousend_posts('гагра'))
 # https://colab.research.google.com/drive/1MKPCVPBsoXHuZGqyzminWGGmxvJu6nwz
