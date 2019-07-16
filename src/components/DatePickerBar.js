47f32f93108de164ff4b216d43737cbb73892874
Almaz <dolg.almaz@yandex.ru>
2019-07-16 13:40:41 +0300

    Add check box to front

--- src/components/DatePickerBar.js
+++ src/components/DatePickerBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Button, DatePicker} from 'antd';
 
 const {RangePicker} = DatePicker;
+import {CheckBox} from "./CheckBox"
 
 const moment = require('moment');
 
@@ -13,7 +14,8 @@ export class DatePickerBar extends React.Component {
     }
 
     componentDidMount() {
-        this.setState({dates: [moment(), moment()]})
+        this.setState({dates: [moment(), moment()].map((v) => v.unix())})
+        this.setState({sr: '', ar: ''})
     }
 
     render() {
@@ -22,23 +24,45 @@ export class DatePickerBar extends React.Component {
             position: 'absolute',
             zIndex: 1000,
             right: '12px',
-            top: '14px'
+            top: '14px',
+            width: '300px',
+            float: 'right',
+            textAlign: 'right'
+        };
+        const DivDivStyle = {
+            margin: '10px 0px 0px 0px',
         };
         return (
             <div style={DivStyle}>
-                <RangePicker
-                    disabledDate={DatePickerBar.disabledDate}
-                    showTime={{
-                        hideDisabledOptions: true
-                    }}
-                    onChange={x => this.onChange(x)}
-                    onOk={x => this.onChange(x)}/>
-                <Button type="primary"
-                        icon="search"
-                        style={{marginLeft: "10px"}}
-                        onClick={() => this.props.onSearch(this.state.dates)}>
-                    Search
-                </Button>
+                <div style={{textAlign: 'center'}}>
+                    <RangePicker
+                        disabledDate={DatePickerBar.disabledDate}
+                        showTime={{
+                            hideDisabledOptions: true
+                        }}
+                        onChange={x => this.onChange(x)}
+                        onOk={x => this.onChange(x)}
+                        format="DD.MM.YY"
+                        separator=" ~ "/>
+                </div>
+                <div style={DivDivStyle}>
+                    <CheckBox dataArray={['Женщины', 'Мужчины', 'Группы']}
+                        style={{width: 300}}
+                        func={e => this.setState({sr: e})}>Выберите половые группы</CheckBox>
+                </div>
+                <div style={DivDivStyle}>
+                    <CheckBox dataArray={['0-14', '15-21', '22-35', '36-50', '51+']}
+                    style={{width: 300}}
+                    func={e => this.setState({ar: e})}>Выберите возрастный группы</CheckBox>
+                </div>
+                <div style={DivDivStyle}>
+                    <Button type="primary"
+                            icon="search"
+                            style={{marginLeft: "10px"}}
+                            onClick={() => this.props.onSearch(this.state.dates, this.state.sr, this.state.ar)}>
+                        Search
+                    </Button>
+                </div>
             </div>
         );
     };
